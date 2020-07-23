8b7f912c16cad74c1838d93c302d54215bc5cffa
J-Haze <59891689+J-Haze@users.noreply.github.com>
2020-07-23 10:28:47 -0700

    Add files via upload
    Added "Tabs" functionality. No tab content yet.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,78 @@
 //Seperate loading the main page and loading the Home tab
 //"Put the contents of each tab within its own module"
 
-//define all divs let lib = document.querySelector('#library');
-let logoContainer = document.()
+import {home} from './homeTab';
+import {menu} from './menuTab';
+// import {reservations} from '.reservationsTab';
+// menu();
+let mode = "home";
 
-// rendering function
+//Add event listeners for the tabs
+
+// let homeTab = document.getElementById('home-tab');
+// let resTab = document.getElementById('reservations-tab');
+// let menuTab = document.getElementById('menu-tab');
+let tabsContainer = document.getElementById("tabs-container")
+
+let homeTab = document.createElement('div');
+let resTab = document.createElement('div');
+let menuTab = document.createElement('div');
+
+homeTab.id = "home-tab";
+homeTab.className = "tab selected";
+homeTab.innerHTML = "HOME";
+tabsContainer.appendChild(homeTab);
+
+
+menuTab.id = "menu-tab";
+menuTab.className = "tab";
+menuTab.innerHTML= "MENU";
+tabsContainer.appendChild(menuTab);
+
+resTab.id = "reservations-tab";
+resTab.className = "tab";
+resTab.innerHTML = "RESERVATIONS";
+tabsContainer.appendChild(resTab);
+
+
+homeTab.addEventListener('click', () => {
+    if (mode == "home") return
+    mode = "home";
+    homeTab.classList.add("selected");
+    menuTab.classList.remove("selected");
+    resTab.classList.remove("selected");
+    home();//Am I calling this function? Is that okay?
+});
+
+menuTab.addEventListener('click', () => {
+    if (mode == "menu") return
+    mode = "menu";
+    homeTab.classList.remove("selected");
+    menuTab.classList.add("selected");
+    resTab.classList.remove("selected");
+    menu(); //Am I calling a function that isn't defined?
+});
+
+resTab.addEventListener('click', () => {
+    if (mode == "res") return
+    mode = "res";
+    homeTab.classList.remove("selected");
+    menuTab.classList.remove("selected");
+    resTab.classList.add("selected");
+    // res(); //Am I calling a function that isn't defined?
+});
+
+home();
+
+
+
+
+// if (mode == "home"){
+//     home();
+// }else if (mode == "menu"){
+//     menu();
+// }else if (mode == "reservations"{
+//     // reservations();
+//     return
+// }
+
--- src/menuTab.js
+++ src/menuTab.js
@@ -1,34 +1,25 @@
-//homeTab.js
-
-
-// const homeTab = () => {
-
-function home() {
-    console.log('Running home() from ./homeTab.js')
+function menu() {
+    console.log('Running menu() from ./menuTab.js')
     // let tabsContainer = document.getElementById("tabs-container")
-    let mode = "home";
 
     // let homeTab = document.createElement('div');
-    // let resTab = document.createElement('div');
-    // let menuTab = document.createElement('div');
-
     // homeTab.id = "home-tab";
-    // homeTab.className = "tab selected";
+    // homeTab.className = "tab";
     // homeTab.innerHTML = "HOME";
     // tabsContainer.appendChild(homeTab);
 
-    
+    // let menuTab = document.createElement('div');
     // menuTab.id = "menu-tab";
-    // menuTab.className = "tab";
+    // menuTab.className = "tab selected";
     // menuTab.innerHTML= "MENU";
     // tabsContainer.appendChild(menuTab);
 
+    // let resTab = document.createElement('div');
     // resTab.id = "reservations-tab";
     // resTab.className = "tab";
-    // resTab.innerHTML = "RESERVATIONS";
+    // resTab.innerHTML = "RESERVATIONS"
     // tabsContainer.appendChild(resTab);
     
-
     let contentContainer = document.getElementById("content-container")
     
     let homeContent = document.createElement('div');
@@ -54,7 +45,6 @@ function home() {
     story.id = "story";
     story.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. <br><br>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. <br><br>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     homeContent.appendChild(story);
-
 }
 
-export{home}
+export{menu}
