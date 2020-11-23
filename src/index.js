ee913102be6d8d6ec424d289492d262b0ef3a068
Justin Hazelton <beaverjustin1@aol.com>
2020-11-22 17:31:10 -0800

    Changed story to Lorem ipsum

--- src/index.js
+++ src/index.js
@@ -1,43 +1,17 @@
-//Use functions and comments
-//Seperate loading the main page and loading the Home tab
-//"Put the contents of each tab within its own module"
-
 import {home} from './homeTab';
 import {menu} from './menuTab';
 import {reservations} from './reservationsTab';
 
 let mode = "home";
 
-//Add event listeners for the tabs
-
-// let tabsContainer = document.getElementById("tabs-container")
 let contentContainer = document.getElementById('content-container');
 let homeTab = document.getElementById('home-tab');
 let resTab = document.getElementById('reservations-tab');
 let menuTab = document.getElementById('menu-tab');
 
 function clear() {
-    // contentContainer.empty();
     contentContainer.innerHTML = "";
 }
-// let homeTab = document.createElement('div');
-// let resTab = document.createElement('div');
-// let menuTab = document.createElement('div');
-
-// homeTab.id = "home-tab";
-// homeTab.className = "tab selected";
-// homeTab.innerHTML = "HOME";
-// tabsContainer.appendChild(homeTab);
-
-// menuTab.id = "menu-tab";
-// menuTab.className = "tab";
-// menuTab.innerHTML= "MENU";
-// tabsContainer.appendChild(menuTab);
-
-// resTab.id = "reservations-tab";
-// resTab.className = "tab";
-// resTab.innerHTML = "RESERVATIONS";
-// tabsContainer.appendChild(resTab);
 
 
 homeTab.addEventListener('click', () => {
@@ -47,7 +21,7 @@ homeTab.addEventListener('click', () => {
     menuTab.classList.remove("selected");
     resTab.classList.remove("selected");
     clear();
-    home();//Am I calling this function? Is that okay?
+    home();
 });
 
 menuTab.addEventListener('click', () => {
@@ -57,7 +31,7 @@ menuTab.addEventListener('click', () => {
     menuTab.classList.add("selected");
     resTab.classList.remove("selected");
     clear();
-    menu(); //Am I calling a function that isn't defined?
+    menu(); 
 });
 
 resTab.addEventListener('click', () => {
@@ -67,22 +41,8 @@ resTab.addEventListener('click', () => {
     menuTab.classList.remove("selected");
     resTab.classList.add("selected");
     clear();
-    reservations(); //Am I calling a function that isn't defined?
+    reservations();
 });
 
 home();
-// menu();
-
-
-
-
-
-// if (mode == "home"){
-//     home();
-// }else if (mode == "menu"){
-//     menu();
-// }else if (mode == "reservations"{
-//     // reservations();
-//     return
-// }
 
--- src/menuTab.js
+++ src/menuTab.js
@@ -1,24 +1,5 @@
 function menu() {
     console.log('Running menu() from ./menuTab.js')
-    // let tabsContainer = document.getElementById("tabs-container")
-
-    // let homeTab = document.createElement('div');
-    // homeTab.id = "home-tab";
-    // homeTab.className = "tab";
-    // homeTab.innerHTML = "HOME";
-    // tabsContainer.appendChild(homeTab);
-
-    // let menuTab = document.createElement('div');
-    // menuTab.id = "menu-tab";
-    // menuTab.className = "tab selected";
-    // menuTab.innerHTML= "MENU";
-    // tabsContainer.appendChild(menuTab);
-
-    // let resTab = document.createElement('div');
-    // resTab.id = "reservations-tab";
-    // resTab.className = "tab";
-    // resTab.innerHTML = "RESERVATIONS"
-    // tabsContainer.appendChild(resTab);
     
     let contentContainer = document.getElementById("content-container")
 
@@ -93,19 +74,12 @@ function menu() {
             ["Assorted Ice Creams & Sorbet", 5]
         ]
     };
-    
-    // let soups = [
-    //     ["House Soup", 8],
-    //     ["Lobster Bisque", 9]
-    // ];
-
-    // let menuSections = [cocktails, soups];
 
     console.log(menus)
     
     let menuContent = document.createElement('div');
     menuContent.id = "menu-content";
-    // menuContent.className = "menu-content";
+
     contentContainer.appendChild(menuContent);
     let col1 = document.createElement('div');
     let col2 = document.createElement('div');
@@ -116,9 +90,6 @@ function menu() {
     col1.className = "col";
     col2.className = "col";
 
-    // console.log(menuSections)
-    // console.log(cocktails)
-    // for (let i = 0; i < menus.length; i++){
     let count = 0;
     for (let i in menus){
         let newSect = document.createElement('div');
@@ -160,39 +131,9 @@ function menu() {
         };
 
         count++;
-        // menuContent.appendChild(newSect);
 
-
-        // console.log("With i", Object.getOwnPropertyNames(menus[i]))
-        // console.log("just i:", i)
-        // console.log("No i", Object.getOwnPropertyNames(menus))
-
-        // let newTable = document.createElement('table')
-        // for (i in sect){
-
-        // }
     }
 
-
-    // contentContainer.appendChild(innerHTML);
-    // let storyHeadline = document.createElement('div');
-    // storyHeadline.id = "story-headline";
-    // storyHeadline.innerHTML = "MENU";
-    // homeContent.appendChild(storyHeadline); 
-
-    // let photoDiv = document.createElement('div');
-    // photoDiv.id = "photoDiv";
-    // homeContent.appendChild(photoDiv); 
-
-    // let homePhoto = document.createElement('img');
-    // homePhoto.id = "landscape-photo";
-    // homePhoto.src = "images/landscape.jpg";
-    // photoDiv.appendChild(homePhoto);
-
-    // let story = document.createElement('div');
-    // story.id = "story";
-    // story.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. <br><br>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. <br><br>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-    // homeContent.appendChild(story);
 }
 
 export{menu}
